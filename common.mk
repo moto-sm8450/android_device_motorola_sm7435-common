0b396a9866c5c00aa1487634091e02b85d656ac8
Marc Bourgoin <themard85@gmail.com>
2025-11-22 19:25:36 -0700

    sm7435-common: Drop single_tap touch gesture
    The kernel changes is causing intermittent touch issues for some RETUS users
    and trying the rest of the kernel module changes from sm8475 hasn't fixed it
    and I haven't seen any other potential fixes and I'm clueless...
    
    Change-Id: If0a1fdaee910af311044f2a5ac451c3272a3f257

--- common.mk
+++ common.mk
@@ -342,8 +342,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch-service.motorola \
-    vendor.lineage.touch-service.moto_sm7435
+    vendor.lineage.touch-service.motorola
 
 $(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
 
